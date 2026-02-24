7b7656f43dd5d8f71bcc66e7851df62d522b3507
rainssmell <rainssmell@gmail.com>
2026-02-24 23:44:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 NOTIFY_BOT_TOKEN = os.getenv("NOTIFY_BOT_TOKEN")
 
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
+SPREADSHEET_ID = "1gw3Fvr7NWg6_R8alqjvG2Ug9iN7BjFAgvMLW4_SkJ8E"
