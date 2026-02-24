5cd11a550b518ed0b062b8d33c50a0ceef10ec95
rainssmell <rainssmell@gmail.com>
2026-02-24 16:02:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-
-# Токен бота уведомлений
 NOTIFY_BOT_TOKEN = os.getenv("NOTIFY_BOT_TOKEN")
+
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
