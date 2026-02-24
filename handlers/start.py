c7c2f054121d2d27d16257a5f1252eda0eed85ea
rainssmell <rainssmell@gmail.com>
2026-02-24 15:25:48 +0300

    start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,13 +1,30 @@
 from aiogram import Router, types
 from aiogram.filters import Command
-from keyboards.packages import packages_kb
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.fsm.context import FSMContext
+
+from handlers.booking import Booking
 
 router = Router()
 
-@router.message(Command("start"))
-async def start(msg: types.Message):
-    await msg.answer(
-        "Привет! Я бот продакшена «Дёшево, сердито».\n\n"
-        "Выбери пакет съёмки:",
-        reply_markup=packages_kb()
+
+def contact_kb():
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="Отправить контакт", request_contact=True)]
+        ],
+        resize_keyboard=True,
+        one_time_keyboard=True
     )
+
+
+@router.message(Command("start"))
+async def start(msg: types.Message, state: FSMContext):
+    await state.clear()
+
+    await msg.answer(
+        "Привет! Перед началом работы отправьте, пожалуйста, ваш контакт.",
+        reply_markup=contact_kb()
+    )
+
+    await state.set_state(Booking.waiting_for_contact)
