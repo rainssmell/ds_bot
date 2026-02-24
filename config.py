c3c0c58398e0bea4798250022f0c5c16eb4034c6
rainssmell <rainssmell@gmail.com>
2026-02-24 15:38:09 +0300

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
+# Токен бота уведомлений
+NOTIFY_BOT_TOKEN = os.getenv("NOTIFY_BOT_TOKEN")
